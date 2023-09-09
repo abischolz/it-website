fd21ac253691259263ceb99642952ff76d459ee9
Abi Scholz <abi.scholz@gmail.com>
2023-09-09 17:19:35 -0400

    Feat: contact form and contact page. Added navigation, server file, minimal styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,17 @@
 <script>
   import NavBar from '$lib/components/NavBar.svelte';
-</script>
+  import HeaderDrip from '$lib/components/Header-Drip.svelte';
+  // color - reactive element 
 
+</script>
+<div class="container mx-auto bg-fixed inset-0 bg-cover bg-no-repeat bg-[top_0%_left_60%] z-0" id="main-container" >
+  <div class="inline-block fixed z-20 align-top mx-10">
+    <h1 class="font-display font-bold text-5xl tracking-wider p-5">Isaac Tecosky</h1>
+  </div>
+  <div class>
+    <HeaderDrip class="z-10" />
+  </div>
+  </div>
 <nav class="hidden md:block">
 
   <a class='font-display' href="/"> Home </a>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,16 +4,21 @@
   import "tailwindcss/tailwind.css"
 
 </script>
-<div class="container mx-auto bg-fixed inset-0 bg-cover bg-no-repeat bg-bottom z-0" id="main-container" >
+<!-- essentially two different designs: one for mobile/tablet, another for computer -->
+
+<div class="hidden md:block">
+  <NavImage />
+</div>
+<!-- this is the mobile block: static background, app-like nav bar, single paint pattern  -->
+
+<div class="container mx-auto bg-fixed inset-0 bg-cover bg-no-repeat bg-[top_0%_left_60%] z-0" id="main-container" >
   <div class="inline-block fixed z-20 align-top mx-10">
     <h1 class="font-display font-bold text-5xl tracking-wider p-5">Isaac Tecosky</h1>
   </div>
   <div>
     <HeaderDrip class="z-10" />
   </div>
-  <div class="hidden md:block">
-    <NavImage />
-  </div>
+
 
 </div>
 
